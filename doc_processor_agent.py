417c1e22d89b6e2716924f6cfba333091ca338a7
Steve Wang <stevewang@alumni.stanford.edu>
2025-06-22 11:13:18 -0400

    Add illusion of thinking paper to data directory;; should be ignored per gitignore

--- doc_processor_agent.py
+++ doc_processor_agent.py
@@ -43,4 +43,4 @@ def generate_filename():
 # display pdf
 # display.IFrame(filepath, width=1000, height=600)
 
-get_paper_from_url(url='https://arxiv.org/pdf/2410.03348', filename="dolphin_paper.pdf")
+get_paper_from_url(url='https://ml-site.cdn-apple.com/papers/the-illusion-of-thinking.pdf', filename="illusion_of_thinking.pdf")
